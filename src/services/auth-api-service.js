b29784120fd6a963e85a9cc60d5f3c179cc1f778
Ryan Nicoletti <ryannicoletti@Ryans-MacBook-Pro-2.local>
2020-08-27 11:32:21 -0400

    deployed to vercel

---
+++ src/services/auth-api-service.js
@@ -0,0 +1,9 @@
+import config from "../config";
+import TokenService from "./token-service";
+import IdleService from "./idle-service";
+
+const authApiService = {
+  postUser(user) {
+    return fetch();
+  },
+};
